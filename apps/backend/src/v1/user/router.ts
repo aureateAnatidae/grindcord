e682b667ca01bc29f0948d85213d33ca6efe2196
aureateAnatidae <34411014+aureateAnatidae@users.noreply.github.com>
2026-01-06 15:44:49 -0700

    refactor: won't need user routes till long time later

---
+++ apps/backend/src/v1/user/router.ts
@@ -0,0 +1,33 @@
+import { getUserPoints } from "@v1/user/service";
+import { Hono } from "hono";
+import { describeRoute, resolver, validator } from "hono-openapi";
+import { z } from "zod";
+
+const app = new Hono();
+
+const UserPoints = z.object({ points: z.int() });
+
+// app.get(
+//     "/:user_id",
+//     describeRoute({
+//         description: "Get a user's profile",
+//         responses: {
+//             200: {
+//                 description: "Successful response",
+//                 content: {
+//                     "application/json": {
+//                         // schema: resolver(UserPoints),
+//                     },
+//                 },
+//             },
+//         },
+//     }),
+//     async (c) => {
+//         const {  user_id } = c.req.param();
+//         // return c.json({ points: await getUserPoints( user_id) });
+//     },
+// );
+// Get a guild's leaderboard
+// app.get("/:guild_id", c);
+
+export default app;
